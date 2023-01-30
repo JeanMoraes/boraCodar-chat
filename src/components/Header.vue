6bb7c1bd80a196c6b6d54673151f97c031fa93c7
JeanMoraes <moraes.jean91@gmail.com>
2023-01-30 16:57:12 -0300

    feat: change profile picture and title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
     <header>
         <div class="container-info">
             <div class="avatar">
-            <img alt="Profile Name..." src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light'/>
+            <img alt="Profile Name..." src='https://github.com/JeanMoraes.png'/>
             </div>
             <div class="profile-info">
                 <p>Jean Moraes</p>
